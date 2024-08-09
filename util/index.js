2cd33652cf9d2ff773859985c6119b3aa12a4788
opopeieie <372392928@qq.com>
2024-08-09 09:54:18 +0000

    add utf-8, add ranking

--- util/index.js
+++ util/index.js
@@ -9,20 +9,38 @@ export const getCodeSnippet = (filePath, lineNumber, context = 0) => {
 };
 
 export const formatOutputJsontoHTMLList = (data) => {
-    let html = '<ul>\n';
+    let html = '<!DOCTYPE html>\n';
+    html += '<html lang="en">\n';
+    html += '<head>\n';
+    html += '    <meta charset="utf-8">\n';
+    html += '    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n';
+    html += '    <title>Formatted Output</title>\n';
+    html += '</head>\n';
+    html += '<body>\n';
+    html += '<ul>\n';
+    const rankAuthors = new Map();
 
     data.forEach(fileData => {
         if (fileData.identifiers.length > 0) {
-            html += `    <li>\n        <strong>File:</strong> ${fileData.file}\n`;
+            html += '    <li>\n';
+            html += `        <strong>File:</strong> ${fileData.file}\n`;
             html += '        <ul>\n';
             fileData.identifiers.forEach(identifier => {
+                rankAuthors.set(identifier.author, (rankAuthors.get(identifier.author) || 0) + 1);
                 html += `            <li><strong>Line:</strong> ${identifier.line} <strong>Snippet:</strong> ${identifier.snippet} <strong>Author:</strong> <span style="background-color: yellow">${identifier.author}</span></li>\n`;
             });
             html += '        </ul>\n';
+            html += '    </li>\n';
         }
-        html += '    </li>\n';
     });
 
     html += '</ul>\n';
+    rankAuthors.forEach((value, key) => {
+        html += `    <div><strong>Author:</strong> ${key} <strong>Count:</strong> ${value}</div>\n`;
+    })
+    html += '</body>\n';
+    html += '</html>\n';
+
     return html;
 };
+
